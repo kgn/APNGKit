c0fd3aa1d4fec6e71e63df1cad12331c65baad53
David Keegan <git@davidkeegan.com>
2021-10-29 19:33:59 -0700

    Add tvOS

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "APNGKit",
-    platforms: [.macOS(.v10_10), .iOS(.v8)],
+    platforms: [.macOS(.v10_10), .iOS(.v8), .tvOS(.v14)],
     products: [
         .library(
             name: "APNGKit",
